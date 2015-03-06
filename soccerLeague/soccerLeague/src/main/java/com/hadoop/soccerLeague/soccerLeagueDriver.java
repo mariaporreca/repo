42d82795cd6ce7c5c1bade3493b90bd04dfbc9bf
Maria Porreca <maria.porreca@globant.com>
2015-03-06 14:25:52 -0300

    newProject

---
+++ soccerLeague/soccerLeague/src/main/java/com/hadoop/soccerLeague/soccerLeagueDriver.java
@@ -0,0 +1,13 @@
+package com.hadoop.soccerLeague;
+
+/**
+ * Hello world!
+ *
+ */
+public class soccerLeagueDriver
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
