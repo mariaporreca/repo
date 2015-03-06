6dc0fb621af2da5b17822da6045cfb408c6185fe
Maria Porreca <maria.porreca@globant.com>
2015-03-06 16:11:18 -0300

    code updated

--- soccerLeague/soccerLeague/src/main/java/com/hadoop/soccerLeague/soccerLeagueDriver.java
+++ soccerLeague/soccerLeague/src/main/java/com/hadoop/soccerLeague/soccerLeagueDriver.java
@@ -1,5 +1,15 @@
 package com.hadoop.soccerLeague;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 /**
  * Created by maria.porreca on 3/6/15.
  *
